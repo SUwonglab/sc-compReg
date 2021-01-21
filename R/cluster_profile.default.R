8f8f2dab39e3882e9cf04a27c02b346f71dfe61c
sophial05 <sophialu@stanford.edu>
2021-01-21 11:14:31 -0800

    cluster_profile has now been dubugged

--- R/cluster_profile.default.R
+++ R/cluster_profile.default.R
@@ -1,4 +1,4 @@
-cluster.profile.default <- function(O1,
+cluster_profile.default <- function(O1,
                             E1,
                             O1.idx,
                             E1.idx,
@@ -17,46 +17,50 @@ cluster.profile.default <- function(O1,
         if (! is(O1, 'matrix')) {
             stop('O1 must be a matrix or sparseMatrix. Please check your format.')
         }
-        O1 = Matrix(O1, sparse = T)
+        O1 <- Matrix(O1, sparse = T)
     }
 
     if (! is(O2, 'sparseMatrix')) {
         if (! is(O2, 'matrix')) {
             stop('O2 must be a matrix or sparseMatrix. Please check your format.')
         }
-        O2 = Matrix(O2, sparse = T)
+        O2 <- Matrix(O2, sparse = T)
     }
 
     if (! is(E1, 'sparseMatrix')) {
         if (! is(E1, 'matrix')) {
             stop('E1 must be a matrix or sparseMatrix. Please check your format.')
         }
-        E1 = Matrix(E1, sparse = T)
+        E1 <- Matrix(E1, sparse = T)
     }
 
     if (! is(E2, 'sparseMatrix')) {
         if ( ! is(E2, 'matrix')) {
             stop('E2 must be a matrix or sparseMatrix. Please check your format.')
         }
-        E2 = Matrix(E2, sparse = T)
+        E2 <- Matrix(E2, sparse = T)
     }
 
     if (! is(O1.idx, 'numeric')) {
         stop('O1.idx must be a vector of integer (indices).')
     }
 
-    O1.idx = as.integer(O1.idx)
-    O1.idx = normalize.index(O1.idx) #scale down with starting index of 0
+    O1.idx <- as.integer(O1.idx)
+    O1.idx <- normalize.index(O1.idx) #scale down with starting index of 0
 
     if (! is(E1.idx, 'numeric') | ! is(E1.idx, 'vector')) {
         stop('E1.idx must be a vector of integer (indices).')
     }
 
-    E1.idx = as.integer(E1.idx)
-    E1.idx = normalize.index(E1.idx) #scale down to have index starting at 0
+    E1.idx <- as.integer(E1.idx)
+    E1.idx <- normalize.index(E1.idx) #scale down to have index starting at 0
 
     if (! is(symb1, 'vector')) {
         stop('symb1 must be a vector of characters.')
+    }
+
+    if (is(pk.name1, 'list')) {
+        pk.name1 <- unlist(pk.name1, use.names = F)
     }
 
     if (! is(pk.name1, 'vector')) {
@@ -68,18 +72,22 @@ cluster.profile.default <- function(O1,
         stop('O2.idx must be a vector of integer (indices).')
     }
 
-    O2.idx = as.integer(O2.idx)
-    O2.idx = normalize.index(O2.idx) #scale down with starting index of 0
+    O2.idx <- as.integer(O2.idx)
+    O2.idx <- normalize.index(O2.idx) #scale down with starting index of 0
 
     if (! is(E2.idx, 'numeric') | ! is(E2.idx, 'vector')) {
         stop('E2.idx must be a vector of integer (indices).')
     }
 
-    E2.idx = as.integer(E2.idx)
-    E2.idx = normalize.index(E2.idx) #scale down to have index starting at 0
+    E2.idx <- as.integer(E2.idx)
+    E2.idx <- normalize.index(E2.idx) #scale down to have index starting at 0
 
     if (! is(symb2, 'vector')) {
         stop('symb2 must be a vector of characters.')
+    }
+
+    if (is(pk.name2, 'list')) {
+        pk.name2 <- unlist(pk.name2, use.names = F)
     }
 
     if (! is(pk.name2, 'vector')) {
@@ -94,63 +102,63 @@ cluster.profile.default <- function(O1,
         stop('pk.name.intersect2 must be a vector of characters.')
     }
 
-    this.call = match.call()
+    this.call <- match.call()
 
-    print('here')
-    K1 = max(max(O1.idx), max(E1.idx))
-    symbol = intersect(Symbol1, Symbol2)
-    f1 = match(symbol, Symbol1)
-    E1.mean = matrix(NA, length(f1), K1)
+    K1 <- max(max(O1.idx), max(E1.idx))
+    symbol <- intersect(symb1, symb2)
+    f1 <- match(symbol, symb1)
+    E1.mean <- matrix(NA, length(f1), K1)
     for (i in 1:K1) {
-        gp = which(E1.idx == i)
+        gp <- which(E1.idx == i)
         if (length(gp) == 0) next
-        E1.mean[, i] = rowMeans(E1[f1, gp])
+        E1.mean[, i] <- Matrix::rowMeans(E1[f1, gp])
     }
 
-    K2 = max(max(O2.idx), max(E2.idx))
-    f2 = match(symbol, Symbol2)
-    E2.mean = matrix(NA, length(f2), K2)
+    K2 <- max(max(O2.idx), max(E2.idx))
+    f2 <- match(symbol, symb2)
+    E2.mean <- matrix(NA, length(f2), K2)
     for (i in 1:K2) {
         gp = which(E2.idx == i)
         if (length(gp) == 0) next
-        E2.mean[, i] = rowMeans(E2[f2, gp])
+        E2.mean[, i] <- Matrix::rowMeans(E2[f2, gp])
     }
 
-    pf1 = match(pk.name.intersect1, pk.name1)
-    pf2 = match(pk.name.intersect2, pk.name2)
-    d1 = match(pk.name1, pk.name.intersect1)
-    d2 = match(pk.name2, pk.name.intersect2)
-    elem.name = rbind(pk.name.intersect1, pk.name1[d1 == 0],
-                      pk.name2[d2 == 0])
-    m = length(pk.name.intersect1)
-    elem.len = length(elem.name)
-    O1.mean = matrix(NA, elem.len, K1)
-    O2.mean = matrix(NA, elem.len, K2)
+    pf1 <- match(pk.name.intersect1, pk.name1)
+    pf2 <- match(pk.name.intersect2, pk.name2)
+    d1 <- is.element(pk.name1, pk.name.intersect1)
+    d2 <- is.element(pk.name2, pk.name.intersect2)
+    elem.name <- c(pk.name.intersect1, pk.name1[d1 == 0],
+                  pk.name2[d2 == 0])
+
+    m <- length(pk.name.intersect1)
+    elem.len <- length(elem.name)
+    O1.mean <- matrix(NA, elem.len, K1)
+    O2.mean <- matrix(NA, elem.len, K2)
 
     for (i in 1:K1) {
         gp = which(O1.idx == i)
         if (length(gp) == 0) next
-        O1.mean[1:m, i] = rowMeans(O1[pf1, gp] > 0)
-        O1.mean[(1+m):sum(d1 == 0), i] = rowMeans(O1[d1 == 0, gp] > 0)
+        O1.mean[1:m, i] <- Matrix::rowMeans(O1[pf1, gp] > 0)
+        O1.mean[(1+m) : (m + sum(d1 == 0)), i] <- Matrix::rowMeans(O1[d1 == 0, gp] > 0)
     }
 
     for (i in 1:K2) {
         gp = which(O2.idx == i)
         if (length(gp) == 0) next
-        O2.mean[1:m, i] = rowMeans(O2[f2, gp] > 0)
-        O2.mean[(m + sum(d1 == 0) + 1) : elem.len, i] = rowMeans(O2[f2 == 0, gp] > 0)
+        O2.mean[1:m, i]  <-  Matrix::rowMeans(O2[pf2, gp] > 0)
+        O2.mean[(m + sum(d1 == 0) + 1) : elem.len, i] = Matrix::rowMeans(O2[d2 == 0, gp] > 0)
     }
 
-    O1.mean = O1.mean / colMeans(O1.mean)
-    O2.mean = O2.mean / colMeans(O2.mean)
+    O1.mean <- O1.mean / Matrix::colMeans(O1.mean)
+    O2.mean = O2.mean / Matrix::colMeans(O2.mean)
 
-    output = list('E1.mean' = E1.mean,
+    output <- list('E1.mean' = E1.mean,
                   'E2.mean' = E2.mean,
                   'symbol' = symbol,
                   'O1.mean' = O1.mean,
                   'O2.mean' = O2.mean,
                   'elem.name' = elem.name)
-    output$call = this.call
+    output$call <- this.call
 
     return(output)
 }
--- comp_reg_sanity.R
+++ comp_reg_sanity.R
@@ -8,7 +8,7 @@ s2 = readMat('/Users/Sophia/Desktop/BioStats/compreg/sample2.mat')
 s1 = readMat('/Users/Sophia/Desktop/BioStats/compreg/sample1.mat')
 
 tic()
-output = cluster.profile(Matrix(s1$O1, sparse=T),
+output = cluster_profile(Matrix(s1$O1, sparse=T),
                Matrix(s1$E1, sparse=T),
                 as.integer(s1$O1.idx) - 1,
                as.integer(s1$E1.idx) - 1,
@@ -20,10 +20,80 @@ output = cluster.profile(Matrix(s1$O1, sparse=T),
                as.integer(s2$E2.idx) - 1,
                as.vector(unlist(s2$Symbol2, use.names = F)),
                as.vector(unlist(s2$PeakName2, use.names = F)),
-                as.vector(pni$vo),
-               as.vector(pni$vt)
+                pni$vo,
+               pni$vt
                )
 toc()
+
+O1 = s1$O1
+E1 = s1$E1
+E1.idx = s1$E1.idx
+O1.idx = s1$O1.idx
+symb1 = s1$Symbol1
+symb2 = s2$Symbol2
+O2 = s2$O2
+E2 = s2$E2
+E2.idx = s2$E2.idx
+O2.idx = s2$E2.idx
+pk.name1 = s1$PeakName1
+pk.name2 = s2$PeakName2
+pk.name.intersect1 = pni$vo
+pk.name.intersect2 = pni$vt
+
+K1 = max(max(O1.idx), max(E1.idx))
+symbol = intersect(symb1, symb2)
+f1 = match(symbol, symb1)
+E1.mean = matrix(NA, length(f1), K1)
+for (i in 1:K1) {
+    gp = which(E1.idx == i)
+    if (length(gp) == 0) next
+    E1.mean[, i] = Matrix::rowMeans(E1[f1, gp])
+}
+
+K2 = max(max(O2.idx), max(E2.idx))
+f2 = match(symbol, symb2)
+E2.mean = matrix(NA, length(f2), K2)
+for (i in 1:K2) {
+    gp = which(E2.idx == i)
+    if (length(gp) == 0) next
+    E2.mean[, i] = Matrix::rowMeans(E2[f2, gp])
+}
+
+pf1 = match(pk.name.intersect1, pk.name1)
+pf2 = match(pk.name.intersect2, pk.name2)
+d1 = is.element(pk.name1, pk.name.intersect1)
+d2 = is.element(pk.name2, pk.name.intersect2)
+print(length(d1))
+print(length(d2))
+pk.name1 = unlist(pk.name1, use.names = F)
+pk.name2 = unlist(pk.name2, use.names=F)
+elem.name = c(pk.name.intersect1, pk.name1[d1 == 0],
+                  pk.name2[d2 == 0])
+
+m = length(pk.name.intersect1)
+elem.len = length(elem.name)
+O1.mean = matrix(NA, elem.len, K1)
+O2.mean = matrix(NA, elem.len, K2)
+
+for (i in 1:K1) {
+    gp = which(O1.idx == i)
+    if (length(gp) == 0) next
+    O1.mean[1:m, i] = Matrix::rowMeans(O1[pf1, gp] > 0)
+    O1.mean[(1+m) : (m + sum(d1 == 0)), i] = Matrix::rowMeans(O1[d1 == 0, gp] > 0)
+}
+
+for (i in 1:K2) {
+    gp = which(O2.idx == i)
+    if (length(gp) == 0) next
+    O2.mean[1:m, i] = Matrix::rowMeans(O2[pf2, gp] > 0)
+    print(sum(d2 == 0))
+    print(elem.len - (m + sum(d1==0) + 1))
+    O2.mean[(m + sum(d1 == 0) + 1) : elem.len, i] = Matrix::rowMeans(O2[d2 == 0, gp] > 0)
+}
+
+O1.mean = O1.mean / Matrix::colMeans(O1.mean)
+O2.mean = O2.mean / Matrix::colMeans(O2.mean)
+
 
 # K1 = max(max(s1$O1.idx), max(s1$E1.idx))
 # K2 = max(max(s2$O2.idx), max(s2$E2.idx))
@@ -46,3 +116,7 @@ write.csv(output$E1.mean, 'E1mean.csv')
 write.csv(output$E2.mean, 'E2mean.csv')
 write.csv(output$O1.mean, 'O1mean.csv')
 write.csv(output$O2.mean, 'O2mean.csv')
+
+pkname = read.table('/Users/Sophia/Desktop/BioStats/compreg/PeakName_intersect.txt', header=F)
+length(pkname$V1)
+length(pkname$V2)
