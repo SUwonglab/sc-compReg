feec24699192a78bfa13908a0a1f442942decf26
sophial05 <sophialu@stanford.edu>
2021-03-15 13:55:10 -0700

    Added peak.name and symbol files.

--- cnmf_example.R
+++ cnmf_example.R
@@ -5,19 +5,28 @@ rm(list = ls())
 # set the path to the document with all of the input files
 # path should end in '/'
 # for example, something like path = 'Desktop/compreg_input/'
-path = './example_data/'
+path <- './example_data/'
 
 
-# to convert the previously generated ```peak_gene_coupling_matrix.txt``` by
-# ```cnmf_process_data.sh``` to D matrix
-# change the directory to where ```peak_gene_coupling_matrix.txt```
-# is stored and run the lines below
-# preprocess.path = './preprocess_data/'
-# load peak.name
-# load symbol
+# # to convert the previously generated ```peak_gene_coupling_matrix.txt``` by
+# # ```cnmf_process_data.sh``` to D matrix
+# # change the directory to where ```peak_gene_coupling_matrix.txt```
+# # is stored and run the lines below
+# preprocess.path <- './preprocess_data/'
+# # load peak.name
+# # load symbol
+# symbol <- as.vector(unlist(read.table(paste(path, 'cnmf_symbol.txt', sep=''),
+#                                          header=F), use.names=F))
+# peak.name <- as.vector(unlist(read.table(paste(path, 'cnmf_peak_name.txt', sep=''),
+#                                       header=F), use.names=F))
+# print('Printing first 5 rows of sample 1 peak name...')
+# print(symbol[1:5])
+# print('Printing first 5 rows of sample 1 symbol...')
+# print(peak.name[1:5])
 # D <- cnmf_load_coupling_matrix(paste(preprocess.path, 'peak_gene_coupling_matrix.txt', sep=''),
 #                                peak.name,
 #                                symbol)
+
 
 cnmf.data <- readRDS(paste(path, 'cnmf_data.rds', sep=''))
 cnmf.output <- cnmf(cnmf.data$PeakO,
@@ -34,10 +43,10 @@ if (!require("Matrix")) install.packages("Matrix")
 library(Matrix)
 # log-transforming E matrix and O matrix
 # sc-compReg requires log-transformed matrices
-cnmf.data$PeakO@x = log2(cnmf.data$PeakO@x + 1)
+cnmf.data$PeakO@x <- log2(cnmf.data$PeakO@x + 1)
 writeMM(cnmf.data$PeakO,
         file=paste(path, 'O.mtx', sep=''))
-cnmf.data$X@x = log2(cnmf.data$X@x + 1)
+cnmf.data$X@x <- log2(cnmf.data$X@x + 1)
 writeMM(cnmf.data$X,
         file=paste(path, 'E.mtx', sep=''))
 
